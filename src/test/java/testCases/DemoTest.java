13a335f094e5e2c0df59c922fff997ba79008fda
Akash-Kumar-Verma <akashv5482@gmail.com>
2024-02-14 08:17:18 +0530

    updated few files

---
+++ src/test/java/testCases/DemoTest.java
@@ -0,0 +1,23 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import pages.basePage;
+
+
+public class DemoTest {
+    
+	 @Test
+	    public void test1() {
+            System.out.println("Hello World");
+            System.out.println("Test case 1 passed.");
+	    }
+	    
+	    @Test
+	    public void test2() {
+            System.out.println("Welcome to Flipkart");
+            System.out.println("Test case 2 passed.");
+	    }
+
+}
+
