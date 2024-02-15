33077a12f2c5965dc10338f773a25324e8baff9b
Akash-Kumar-Verma <akashv5482@gmail.com>
2024-02-15 10:38:30 +0530

    updated test case

--- src/test/java/testCases/DemoTest.java
+++ src/test/java/testCases/DemoTest.java
@@ -1,23 +1,45 @@
 package testCases;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import pages.basePage;
 
 
 public class DemoTest {
-    
-	 @Test
+	public static WebDriver driver;
+//	    @Test
+//	    public void test1() {
+//            System.out.println("Hello World");
+//            System.out.println("Test case 1 passed.");
+//	    }
+//	    
+//	    @Test
+//	    public void test2() {
+//            System.out.println("Welcome to Flipkart");
+//            System.out.println("Test case 2 passed.");
+//	    }
+	    @Test
 	    public void test1() {
-            System.out.println("Hello World");
+	    	// setting path for Chrome driver
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+
+			// initializing driver as Chrome driver
+			driver = new ChromeDriver();
+
+			// Maximize the window
+			driver.manage().window().maximize();
+			
+			driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+			// navigating to application
+			driver.get("https://www.amazon.in/");
+			System.out.println("Title"+driver.getTitle());
             System.out.println("Test case 1 passed.");
 	    }
 	    
-	    @Test
-	    public void test2() {
-            System.out.println("Welcome to Flipkart");
-            System.out.println("Test case 2 passed.");
-	    }
+	  
 
 }
 
